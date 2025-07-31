cad91e157011cd919689cdbb2aac43fb733ce7e0
Parichu <orgaaume7741@gmail.com>
2025-07-31 10:08:57 +0700

    Config all file

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -2,32 +2,49 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
+
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command
 from launch.actions import RegisterEventHandler
-from launch.event_handler import OnProcessStart
+from launch.event_handlers import OnProcessStart
 
 from launch_ros.actions import Node
 
+
+
 def generate_launch_description():
 
-    package_name ='myfirstbot'
+
+    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
+    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
+
+    package_name='nbt_bot' #<--- CHANGE ME
 
     rsp = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory(package_name),'launch','rsp.launch.py'
-        )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
     )
+
+    # joystick = IncludeLaunchDescription(
+    #             PythonLaunchDescriptionSource([os.path.join(
+    #                 get_package_share_directory(package_name),'launch','joystick.launch.py'
+    #             )])
+    # )
+
 
     twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
     twist_mux = Node(
-        package="twist_mux",
-        executable="twist_mux",
-        parameters=[twist_mux_params],
-        remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
-    )
+            package="twist_mux",
+            executable="twist_mux",
+            parameters=[twist_mux_params],
+            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
+        )
+
+    
+
 
     robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
 
@@ -40,12 +57,124 @@ def generate_launch_description():
                     controller_params_file]
     )
 
-    delayed_controller_manager = TimerAction(period=3.0, action=[controller_manager])
+    delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
 
-    diff_driver_spawner = Node(
+    diff_drive_spawner = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["diff_cont"],
     )
 
-    delayed_diff_driver_spawner
+    delayed_diff_drive_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[diff_drive_spawner],
+        )
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+    )
+
+    #Spawn imu_sensor_broadcaser
+    # imu_broadcaster_spawner = Node(
+    #     package='controller_manager',
+    #     executable='spawner',
+    #     arguments=['imu_broadcaster']
+    # )
+
+    delayed_joint_broad_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[joint_broad_spawner],
+        )
+    )
+
+    pub_joint_state = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        parameters=[{'use_sim_time' : False}]
+    )
+
+    # imu_driver_node = Node(
+    #     package='icm20948_ros2',         
+    #     executable='imu_node',           
+    #     name='icm20948_driver',
+    #     output='screen'
+    # )
+
+    # imu_filter_node = Node(
+    #     package='imu_filter_madgwick',
+    #     executable='imu_filter_madgwick_node',
+    #     name='imu_filter',
+    #     parameters=[{
+    #         'use_mag': True,
+    #         'use_magnetic_field_msg': True,
+    #         'publish_tf': False,
+    #         'world_frame': 'enu',
+    #         'orientation_stddev': 0.05,
+    #         'gain': 0.01
+    #     }],
+    #     remappings=[
+    #         ('imu/data_raw', '/imu/data_raw'),
+    #         ('imu/data', '/imu/data')
+    #     ]
+    # )
+
+    # ekf_node = Node(
+    #     package='robot_localization',
+    #     executable='ekf_node',
+    #     name='ekf_filter_node',
+    #     output='screen',
+    #     parameters=[os.path.join(get_package_share_directory(package_name), 'config', 'ekf.yaml')],
+    #     # remappings=[
+    #     #     ('/odometry/filtered', '/odom')  # <--- remap ไปที่ /odom
+    #     # ]
+    # )
+
+    # node_laser_link = Node(
+    #     package='tf2_ros',
+    #     executable='static_transform_publisher',
+    #     name='base_to_laser',
+    #     arguments=[
+    #         '0.22', '0', '0.15',
+    #         '0', '3.14159', '3.14159',
+    #         'chassis', 'laser'
+    #     ],
+    #     output='screen'
+    # )
+
+    # Code for delaying a node (I haven't tested how effective it is)
+    # 
+    # First add the below lines to imports
+    # from launch.actions import RegisterEventHandler
+    # from launch.event_handlers import OnProcessExit
+    #
+    # Then add the following below the current diff_drive_spawner
+    # delayed_diff_drive_spawner = RegisterEventHandler(
+    #     event_handler=OnProcessExit(
+    #         target_action=spawn_entity,
+    #         on_exit=[diff_drive_spawner],
+    #     )
+    # )
+    #
+    # Replace the diff_drive_spawner in the final return with delayed_diff_drive_spawner
+
+    # Launch them all!
+    return LaunchDescription([
+        rsp,
+        # joystick,
+        twist_mux,
+        delayed_controller_manager,
+        delayed_diff_drive_spawner,
+        delayed_joint_broad_spawner,
+        # imu_broadcaster_spawner,
+        pub_joint_state,
+        # imu_driver_node,
+        # imu_filter_node,
+        # ekf_node,
+        # node_laser_link,
+    ])
